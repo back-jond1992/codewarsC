4fc7e84a6292290c4c1ef8bfb06bb9c9e71db0db
back-jond1992 <b.jond7@outlook.com>
2022-02-23 15:53:58 +0000

    finishes disemvowel kata

--- 7kyu.c
+++ 7kyu.c
@@ -17,3 +17,30 @@ size_t get_count(const char *s)
   
   return counter;
 }
+
+// Disemvowel Trolls
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *disemvowel(const char *str)
+{
+  
+  int length = strlen(str);
+
+  char *result = malloc(length* sizeof(char) + 1); 
+  
+  int counter = 0;
+  
+  for(int i = 0; i < length; i++) {
+    if(str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u' && str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U') {
+      result[counter] = str[i];
+      counter ++;
+    } 
+  }
+  
+  result[counter] = '\0';
+  
+  return result;
+}
